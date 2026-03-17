ffe4469ec636756fb91231ebb76e799507dc64c6
Matthías Guðni <99895907+MatthiasGudni@users.noreply.github.com>
2026-03-17 11:09:33 +0000

    Eksempler

--- example.py
+++ example.py
@@ -9,9 +9,10 @@ def plot():
         S0=999).run(days=100, showRe=True,
         output="si_example.png")
 
+
     SIR(N=1000, beta=0.5, gamma=0.05, I0=1,
         S0=999, R0=0).run(days=200, showRe=True,
-        output="severe_epidemic.png")
+        output="sir_epidemic.png")
 
 
     SIRDemography(N=1000, Alpha=10, beta=0.3, gamma=0.1,
